a5c42ddd41daa3c558c69f41e80033b3638e2a4e
PowPingDone <ppdplayer@gmail.com>
2017-06-21 17:16:41 +0000

    Add nn

--- audio/nn.py
+++ audio/nn.py
@@ -19,4 +19,33 @@ for x in glob.glob(os.getcwd()+'/'+traindir+'/'+'*.wav'):
             out += [np.sum(x)//len(x)]
         else:
             out += x
-data = np.array(out)
+data = np.array(out,dtype=np.int16)
+
+#------Create/Load model
+if os.path.isfile('model.h5'):
+    print('loading model on disk...')
+    from keras.models import load_model
+    model = load_model('model.h5')
+else:
+    print('creating new model...')
+    from keras.models import Sequential
+    from keras.layers import Conv1D,Activation,MaxPooling1D,LSTM,Dense,Dropout
+    from keras.optimzers import RMSprop
+    model = Sequential()
+    model.add(Conv1D(32,10,padding='same',input_shape=(None,1)))
+    model.add(Activation('relu'))
+    model.add(Conv1D(32,10))
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(pool_size=5))
+    model.add(Dropout(0.2))
+    model.add(Conv1D(64,10,padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv1D(64,10))
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(pool_size=5))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(LSTM(128))
+    model.add(Dropout(0.35))
+    model.add(Dense(1))
+    model.compile(optimizer = RMSprop())
