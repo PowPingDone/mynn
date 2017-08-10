af7990f3fedff6a230b378a63b84a2d314266f22
PowPingDone <ppdplayer@gmail.com>
2017-08-10 20:42:44 +0000

    baab

--- audio/nn.py
+++ audio/nn.py
@@ -16,6 +16,7 @@ if os.path.isfile('preds.npy') and os.path.isfile('wavfiles.npy'):
     Y = np.load('preds.npy')
 else:
     import sys,glob,librosa
+    from tqdm import tqdm
     print('create mega array of wavfiles')
     if os.path.isfile('cache.npy'):
         print('load wavfile cache')
@@ -25,16 +26,12 @@ else:
         out = np.array([])
         for x in glob.glob(os.getcwd()+'/'+traindir+'/*.wav'):
             print('loading file '+x.split('/')[-1])
-            tmp = librosa.load(x,mono=True)[0]
-            print('proccessing file '+x.split('/')[-1])
+            tmp = librosa.load(x,sr=None)[0]
             out = np.append(out,tmp)
         print('saving cached wavfiles')
         np.save('cache',out)
     print('create mega array')
-    data = np.array([np.arange(128)])
-    for x in range(0,len(out)-1,4):
-        data= np.concatenate((data,np.array([out[x:x+128]])))
-    data = np.array(np.delete(data,0))
+    data = np.array([out[x:x+128] for x in tqdm(range(0,len(out)-1,4))])
     np.save('wavfiles',data)
     print('create preds')
     Y = np.array([[x] for x in out[0][129:len(out):4]])
@@ -51,16 +48,17 @@ else:
     print('creating new model...')
     from keras.models import Sequential
     from keras.layers import LSTM,Dense,Dropout
-    from keras.optimizers import RMSprop
+    from keras.layers.embeddings import Embedding
     model = Sequential()
-    model.add(LSTM(128, input_shape = data.shape, return_sequences=False))
+    model.add(Embedding(129,256,input_length=128)))
+    model.add(LSTM(256))
     model.add(Dropout(0.4))
     model.add(Dense(1))
-    model.compile(optimizer = RMSprop(lr = 0.009), loss = 'categorical_crossentropy')
+    model.compile(optimizer = 'rmsprop', loss = 'categorical_crossentropy')
 
 #------♪ AI Train ♪
 itermax = 100
 from keras.callbacks import ModelCheckpoint
 callback = [ModelCheckpoint('model.h5', monitor='val_acc', verbose=1, save_best_only=True, mode='max')]
-model.fit(data, Y, verbose=1, epochs=itermax, batch_size=1, callbacks=callback)
+model.fit(data, Y, verbose=1, epochs=itermax, batch_size=30, callbacks=callback)
 print('exiting at',str(x),'iterations')
