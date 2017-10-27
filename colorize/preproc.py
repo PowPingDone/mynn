5fb514cc1619584271997acdcbe66d1df6e923c5
PowPingDone <ppdplayer@gmail.com>
2017-10-27 10:11:18 -0500

    Get input data
    Former-commit-id: 3588eaa43f7fb1c9131789708823dda0920e0aa2

--- colorize/preproc.py
+++ colorize/preproc.py
@@ -1,2 +1,30 @@
 #------Config
 traindir = 'data'
+size = (1024,1024)
+
+#------Imports
+from keras.preprocessing.image import img_to_array,load_img
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+
+#------Process input data into indata.npy
+data = np.zeros((1)+size,dtype=np.float32)
+buffer = []
+inc = 0
+start = True
+for x in tqdm(glob('./'+traindir+'/*')):
+  try:
+    print('processing '+x.split('/')[-1])
+    buffer += img_to_array(load_img(x,grayscale=True,target_size=size))
+    inc += 1
+    if start:
+      data = np.delete(data,0,0)
+    if inc==15000:
+      inc = 0
+      data = np.concatenate((data,buffer))
+      buffer = []
+  except:
+    print('could not process '+x.split('/')[-1])
+np.save('indata',data)
+del data,buffer
